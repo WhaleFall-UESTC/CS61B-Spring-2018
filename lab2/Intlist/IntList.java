3ca6ee9a0c386eb023e96cddd7c86f45207dd4e6
WhaleFall <166845494@qq.com>
2024-07-09 10:25:07 +0800

    Fixed the bug of null pointer

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
         IntList res = A;
 
         while (A.rest != null) {
@@ -96,6 +99,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
         IntList res = new IntList(A.first, null);
         IntList ptr = res;
         A = A.rest;
--- lab2/Intlist/IntListTest.java
+++ lab2/Intlist/IntListTest.java
@@ -66,4 +66,13 @@ public class IntListTest {
         assertEquals(IntList.of(1, 2, 3), A);
     }
 
+    @Test
+    public void testAll() {
+        testCatenate();
+        testDcatenate();
+        testdSquareList();
+        testList();
+        testSquareListRecursive();
+    }
+
 }
